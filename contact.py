1b415b15b24b2ad765b6c5cc2b143376e390d161
Naithen H <nathan.arcamone@gmail.com>
2022-04-20 22:01:31 +0200

    premier dossier

--- contact.py
+++ contact.py
@@ -1,22 +1,127 @@
-﻿repertoire={
+﻿from tkinter import *
+from tkinter import messagebox
+
+fenetre=Tk()
+
+fenetre.title('Contacts')
+fenetre.minsize(width=350, height=75)
+fenetre.config(pady=20)
+
+
+repertoire={
     "thomas" : "0625456905",
     "sebastien" : "0780685851",
     "pierre": "0625849637",
     'henri': '0497853475',
     "paul" : "0687953412",
 }
-info=input("cherché: ")
+
+label=Label(fenetre,text="Nom ou numéros: ")
+
+def recherche():
+    info=entree.get()
+    print(info)
+    if info > "0" and info<="9999999999":
+        for cle,value in repertoire.items():
+            if value == info:
+                print(info+" est le num de "+cle)
+                messagebox.showinfo("Contact", info+" est le numéro de "+cle)
+                return cle
+        return "Pas de contact a ce numero.", messagebox.showerror("Error","Pas de contact a ce numero.")
+    else:
+        num = repertoire.get(info)
+        if num==None:
+            return "Pas de contact a ce nom.", messagebox.showerror("Error","Pas de contact a ce nom.")
+        else:
+            print("le numero de "+info+" est "+num)
+            messagebox.showinfo("Contact", "Le numéro de "+info+" est "+num)
+            return num
+
+entree=Entry(fenetre,width=30)
+
+recherche_button=Button(fenetre, text="Chercher", command=recherche)
 
 def ajout_contact():
-    name = input("nom du contact:")
-    num= input("numero du contact: ")
-    repertoire[name]=num
-    return repertoire
+    name = ajout_entree_nom.get()
+    num= ajout_entree_num.get()
+    if num < "0600000000":
+        return "Numéro incorrect.", messagebox.showerror("Error","Numéro incorrect.")
+    else:
+        repertoire[name]=num
+        messagebox.showinfo("Success","contact ajouté avec succès")
+        return repertoire,
 
-def supr_contact(information,dico):
+
+ajout_label=Label(fenetre,text="Ajouter un contact")
+nom_ajout_label=Label(fenetre,text="Nom: ")
+ajout_entree_nom=Entry(fenetre, width=10)
+num_ajout_label=Label(fenetre,text="Numéros: ")
+ajout_entree_num=Entry(fenetre, width=10)
+ajout_button=Button(fenetre, text="Ajouter", command=ajout_contact)
+
+def supr_contact():
+    dico=choix_dico_1.get()
+    information=supr_entree_nom.get()
     del dico[information]
     print(dico)
     return "contact suprimer"
+
+supr_label=Label(fenetre,text="Supprimer un contact")
+nom_supr_label=Label(fenetre,text="Nom: ")
+supr_entree_nom=Entry(fenetre, width=10)
+supr_button=Button(fenetre, text="Supprimer", command=supr_contact)
+
+repertoire2=[]
+v = StringVar()
+v.set(repertoire)
+choix_dico_1 = Radiobutton (fenetre, text="repeghghghrtoire", variable=v, value=repertoire)
+
+
+def affiche():
+    liste=[]
+    for cle,value in repertoire.items():
+        liste.append([cle,value])
+    messagebox.showinfo("Contacts",liste)
+    print(liste)
+    return(liste)
+
+affiche_button=Button(fenetre, text="Afficher contacts", command=affiche)
+
+
+
+label.pack()
+entree.pack()
+recherche_button.pack()
+ajout_label.pack(pady=20)
+nom_ajout_label.pack()
+ajout_entree_nom.pack()
+num_ajout_label.pack()
+ajout_entree_num.pack()
+ajout_button.pack()
+
+supr_label.pack(pady=20)
+nom_supr_label.pack()
+supr_entree_nom.pack()
+supr_button.pack()
+choix_dico_1.pack()
+
+affiche_button.pack(pady=20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ##print(supr_contact(info,repertoire))
 
@@ -36,20 +141,7 @@ def supr_contact(information,dico):
 ##            return cle
 ##    return "Pas de contact a ce numero."
 
-def recherche(info):
-    if info > "0" and info<="9999999999":
-        for cle,value in repertoire.items():
-            if value == info:
-                print(info+"est le num de"+cle)
-                return cle
-        return "Pas de contact a ce numero."
-    else:
-        num = repertoire.get(info)
-        if num==None:
-            return "Pas de contact a ce nom."
-        else:
-            print("le numero de "+info+" est "+num)
-            return num
+
 
 ##def modifier(repertoire, info):
 ##    quoi=input("suprimer ou modifier: ")
@@ -60,3 +152,4 @@ def recherche(info):
 ##        return "ok"
 
 
+fenetre.mainloop()
